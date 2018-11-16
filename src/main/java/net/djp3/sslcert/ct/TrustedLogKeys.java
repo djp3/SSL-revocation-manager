44675826182296a022cb526cc25ec24ca9174afe
Don Patterson <d_j_p_3@djp3.net>
2018-11-16 10:43:29 -0800

    Add copyright to a few files

--- src/main/java/net/djp3/sslcert/ct/TrustedLogKeys.java
+++ src/main/java/net/djp3/sslcert/ct/TrustedLogKeys.java
@@ -1,3 +1,24 @@
+/*
+	Copyright 2007-2018
+		Donald J. Patterson 
+*/
+/*
+	This file is part of SSL Revocation Manager , i.e. "SSLRM"
+
+    SSLRM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SSLRM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SSLRM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package net.djp3.sslcert.ct;
 
 /**
